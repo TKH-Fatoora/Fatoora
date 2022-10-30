62a0809d88b54753920c7cc17a2d0f0595f6cd70
Nour <ns00149@tkh.edu.eg>
2022-10-30 20:19:45 +0200

    Nour - Footer done

---
+++ Website/Templates/footer.php
@@ -0,0 +1,28 @@
+<link rel="stylesheet" href="/CSS/footerstyles.css">
+
+
+<footer>
+
+  <div class="footer">
+
+      <!-- quick links -->
+      <div class="box">
+          <h3 class="footerSubT">Quick links</h3>
+          <a href="home.php">Home</a>
+          <a href="scan.php">Scan</a>
+          <a href="history.php">Reciepts</a>
+          <a href="contactus.php">Contact Us</a>
+      </div>
+
+      <!-- extra links -->
+      <div class="box">
+          <h3 class="footerSubT">Extra links</h3>
+          <a href="linkedin.com">LinkedIn</a>
+          <a href="facebook.com">Facebook</a>
+          <a href="#">Careers</a>
+          <a href="https://github.com/TKH-Fatoora/Fatoora">Github</a>
+      </div>
+
+    </div>
+
+</footer>
