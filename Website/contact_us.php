778bcdd64d24b7921993f9fe2522d08b8372c5df
ZiadAmin1212343244 <90804965+ZiadAmin1212343244@users.noreply.github.com>
2022-10-31 18:20:07 +0200

    Added contact us page

---
+++ Website/contact_us.php
@@ -0,0 +1,46 @@
+<?php
+include 'Templates/navbar.php';
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Contact Us</title>
+    <link rel="stylesheet"  href="CSS/contact_us.css">
+</head>
+
+<body>
+    <div>
+    <h1 id="page_title">Contact us</h1>
+    <hr class="horizon_line">
+    </div>
+    
+
+    <div class="form_template">
+        <form>
+    
+
+            <label>Name:</label>
+            <input type="text" id="name" name="name">
+
+            <label>Email:</label>
+            <input type="text" id="email" name="email">
+
+            <label>Subject</label>
+            <input type="text" id="subject" name="subject">
+
+            <label>Message:</label>
+            <textarea id="message" name="message"></textarea>
+
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+
+</body>
+
+</html>
+
+<?php
+include 'Templates/footer.php';
+?>
