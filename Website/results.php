1375bc4dc5b660270d934939525eebc8713df7c5
Nour <ns00149@tkh.edu.eg>
2022-11-02 11:51:09 +0200

    Nour: results page

---
+++ Website/results.php
@@ -0,0 +1,207 @@
+<?php
+include 'Templates/navbar.php';
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="CSS/resultsstyles.css">
+    <title></title>
+  </head>
+  <body>
+    <section class="results">
+      <h1>Results</h1>
+      <div class="container">
+        <div class="box">
+          <div class="name">
+            <h2>name 1</h2>
+          </div>
+          <table class="table">
+            <tr class="tabletitle">
+              <td class="item"><h2>Item</h2></td>
+              <td class="Hours"><h2>Qty</h2></td>
+              <td class="Rate"><h2>Sub Total</h2></td>
+            </tr>
+
+            <tr class="rowitem">
+              <td class="tableitem"><p class="itemtext">Communication</p></td>
+              <td class="tableitem"><p class="itemtext">5</p></td>
+              <td class="tableitem"><p class="itemtext">$375.00</p></td>
+            </tr>
+
+            <tr class="rowitem">
+              <td class="tableitem"><p class="itemtext">Asset Gathering</p></td>
+              <td class="tableitem"><p class="itemtext">3</p></td>
+              <td class="tableitem"><p class="itemtext">$225.00</p></td>
+            </tr>
+
+            <tr class="rowitem">
+              <td class="tableitem"><p class="itemtext">Design Development</p></td>
+              <td class="tableitem"><p class="itemtext">5</p></td>
+              <td class="tableitem"><p class="itemtext">$375.00</p></td>
+            </tr>
+
+            <tr class="rowitem">
+              <td class="tableitem"><p class="itemtext">Animation</p></td>
+              <td class="tableitem"><p class="itemtext">20</p></td>
+              <td class="tableitem"><p class="itemtext">$1500.00</p></td>
+            </tr>
+
+            <tr class="rowitem">
+              <td class="tableitem"><p class="itemtext">Animation Revisions</p></td>
+              <td class="tableitem"><p class="itemtext">10</p></td>
+              <td class="tableitem"><p class="itemtext">$750.00</p></td>
+            </tr>
+
+
+            <tr class="tabletitle">
+              <td></td>
+              <td class="Rate"><h2>taxes</h2></td>
+              <td class="payment"><h2>$419.25</h2></td>
+            </tr>
+            <tr class="tabletitle">
+              <td></td>
+              <td class="Rate"><h2>services</h2></td>
+              <td class="payment"><h2>$419.25</h2></td>
+            </tr>
+
+            <tr class="tabletitle">
+              <td></td>
+              <td class="Rate"><h2>Total</h2></td>
+              <td class="payment"><h2>$3,644.25</h2></td>
+            </tr>
+
+          </table>
+        </div>
+        <div class="box">
+          <div class="name">
+            <h2>name 2</h2>
+          </div>
+          <table class="table">
+            <tr class="tabletitle">
+              <td class="item"><h2>Item</h2></td>
+              <td class="Hours"><h2>Qty</h2></td>
+              <td class="Rate"><h2>Sub Total</h2></td>
+            </tr>
+
+            <tr class="rowitem">
+              <td class="tableitem"><p class="itemtext">Communication</p></td>
+              <td class="tableitem"><p class="itemtext">5</p></td>
+              <td class="tableitem"><p class="itemtext">$375.00</p></td>
+            </tr>
+
+            <tr class="rowitem">
+              <td class="tableitem"><p class="itemtext">Asset Gathering</p></td>
+              <td class="tableitem"><p class="itemtext">3</p></td>
+              <td class="tableitem"><p class="itemtext">$225.00</p></td>
+            </tr>
+
+            <tr class="rowitem">
+              <td class="tableitem"><p class="itemtext">Design Development</p></td>
+              <td class="tableitem"><p class="itemtext">5</p></td>
+              <td class="tableitem"><p class="itemtext">$375.00</p></td>
+            </tr>
+
+            <tr class="rowitem">
+              <td class="tableitem"><p class="itemtext">Animation</p></td>
+              <td class="tableitem"><p class="itemtext">20</p></td>
+              <td class="tableitem"><p class="itemtext">$1500.00</p></td>
+            </tr>
+
+            <tr class="rowitem">
+              <td class="tableitem"><p class="itemtext">Animation Revisions</p></td>
+              <td class="tableitem"><p class="itemtext">10</p></td>
+              <td class="tableitem"><p class="itemtext">$750.00</p></td>
+            </tr>
+
+            <tr class="tabletitle">
+              <td></td>
+              <td class="Rate"><h2>taxes</h2></td>
+              <td class="payment"><h2>$419.25</h2></td>
+            </tr>
+            <tr class="tabletitle">
+              <td></td>
+              <td class="Rate"><h2>services</h2></td>
+              <td class="payment"><h2>$419.25</h2></td>
+            </tr>
+
+            <tr class="tabletitle">
+              <td></td>
+              <td class="Rate"><h2>Total</h2></td>
+              <td class="payment"><h2>$3,644.25</h2></td>
+            </tr>
+
+          </table>
+        </div>
+        <div class="box">
+          <div class="name">
+            <h2>name 3</h2>
+          </div>
+          <table class="table">
+            <tr class="tabletitle">
+              <td class="item"><h2>Item</h2></td>
+              <td class="Hours"><h2>Qty</h2></td>
+              <td class="Rate"><h2>Sub Total</h2></td>
+            </tr>
+
+            <tr class="rowitem">
+              <td class="tableitem"><p class="itemtext">Communication</p></td>
+              <td class="tableitem"><p class="itemtext">5</p></td>
+              <td class="tableitem"><p class="itemtext">$375.00</p></td>
+            </tr>
+
+            <tr class="rowitem">
+              <td class="tableitem"><p class="itemtext">Asset Gathering</p></td>
+              <td class="tableitem"><p class="itemtext">3</p></td>
+              <td class="tableitem"><p class="itemtext">$225.00</p></td>
+            </tr>
+
+            <tr class="rowitem">
+              <td class="tableitem"><p class="itemtext">Design Development</p></td>
+              <td class="tableitem"><p class="itemtext">5</p></td>
+              <td class="tableitem"><p class="itemtext">$375.00</p></td>
+            </tr>
+
+            <tr class="rowitem">
+              <td class="tableitem"><p class="itemtext">Animation</p></td>
+              <td class="tableitem"><p class="itemtext">20</p></td>
+              <td class="tableitem"><p class="itemtext">$1500.00</p></td>
+            </tr>
+
+            <tr class="rowitem">
+              <td class="tableitem"><p class="itemtext">Animation Revisions</p></td>
+              <td class="tableitem"><p class="itemtext">10</p></td>
+              <td class="tableitem"><p class="itemtext">$750.00</p></td>
+            </tr>
+
+
+            <tr class="tabletitle">
+              <td></td>
+              <td class="Rate"><h2>taxes</h2></td>
+              <td class="payment"><h2>$419.25</h2></td>
+            </tr>
+            <tr class="tabletitle">
+              <td></td>
+              <td class="Rate"><h2>services</h2></td>
+              <td class="payment"><h2>$419.25</h2></td>
+            </tr>
+
+            <tr class="tabletitle">
+              <td></td>
+              <td class="Rate"><h2>Total</h2></td>
+              <td class="payment"><h2>$3,644.25</h2></td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </section>
+    <div class="done">
+    <button type="button" name="button"><a href="history.php">Done</a></button>
+  </div>
+  </body>
+</html>
+
+ <?php
+include 'Templates/footer.php';
+  ?>
