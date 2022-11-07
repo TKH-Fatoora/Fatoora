d77f4cb317e5260fa3a29512d1767d22bb8e6687
Nour <ns00149@tkh.edu.eg>
2022-11-08 01:54:23 +0200

    Nour: - Contact Page PHP & SQL - Contact page CSS changes

--- Website/contact_us.php
+++ Website/contact_us.php
@@ -1,6 +1,49 @@
 <?php
-include 'Templates/config.php';
+// connection to databse
+@include 'Templates/config.php';
+
+// start user session
+session_start();
+
+// fetching the value for the user id
+$user_id = $_SESSION['user_id'];
+
+// _____________________________________________________________________________
+
+// if user id is not set, then:
+if(!isset($user_id)){
+  // redirect user to log in again
+   header('location:login.php');
+};
+
+// _____________________________________________________________________________
+
+
+// of the send button is pressed,
+if(isset($_POST['submit'])){
+    // mysqli_real_escape_string() function escapes special characters in a string and prevents against sql attacks
+
+    // fetch the name, email, number & message content of the user
+    $email = mysqli_real_escape_string($conn, filter_var($_POST['email'], FILTER_SANITIZE_STRING));
+    $subject = mysqli_real_escape_string($conn, filter_var($_POST['subject'], FILTER_SANITIZE_STRING));
+    $content = mysqli_real_escape_string($conn, filter_var($_POST['message'], FILTER_SANITIZE_STRING));
+
+// _____________________________________________________________________________
+    // check if a message with the same user info and content already exists in the message table in the database
+    $select_message = mysqli_query($conn, "SELECT * FROM `message` WHERE email = '$email' AND subject = '$subject' AND content = '$content'") or die('query failed');
+
+    // if more than 0 rows were returned, then the message already exists
+    if(mysqli_num_rows($select_message) > 0){
+      $message[] = 'Message sent already!'; // store notification message
+
+    }else{
+      // insert new message in the message table in the databse
+      mysqli_query($conn, "INSERT INTO `message`(UserID, email, subject,  content) VALUES('1', '$email', '$subject', '$content')") or die('query failed');
+      $message[] = 'Message sent successfully!'; // store notification message
+    }
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 
@@ -11,28 +54,27 @@ include 'Templates/config.php';
 </head>
 
 <body>
+
+  <?php include 'Templates/notification.php' ?>
   <?php include 'Templates/navbar.php';?>
+
     <div>
     <h1 id="page_title">Contact us</h1>
-    <hr class="horizon_line">
     </div>
 
     <div class="form_template">
-        <form>
-
-            <label>Name:</label>
-            <input type="text" id="name" name="name">
+        <form action="contact_us.php" method="POST">
 
             <label>Email:</label>
-            <input type="text" id="email" name="email">
+            <input type="email" id="email" name="email" required>
 
             <label>Subject</label>
-            <input type="text" id="subject" name="subject">
+            <input type="text" id="subject" name="subject" required>
 
             <label>Message:</label>
-            <textarea id="message" name="message"></textarea>
+            <textarea required id="message" name="message" cols="30" rows="10"></textarea>
 
-            <input type="submit" value="Submit">
+            <input type="submit" name="submit" value="Submit">
         </form>
     </div>
     <?php include 'Templates/footer.php';?>
