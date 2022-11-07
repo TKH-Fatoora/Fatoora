e07c3ce5403c2c8cae1b6522b0ce4484de815c55
Nour <ns00149@tkh.edu.eg>
2022-11-07 22:43:31 +0200

    Nour & Ahmed Basem: - Log In PHP & SQL - MD5 password hashing applied - input sanitization
    Co-Authored-By: null <AhmeddBasemm@users.noreply.github.com>

--- Website/login.php
+++ Website/login.php
@@ -1,3 +1,68 @@
+<?php
+// connection to database
+@include 'Templates/config.php';
+
+// start user session
+session_start();
+
+// _____________________________________________________________________________
+
+// if the submit(login) button is pressed,
+if(isset($_POST['submit'])){
+  // These two functions are important for extra security purpose in the signup form:
+  // The FILTER_SANITIZE_STRING filter removes tags and remove or encode special characters from a string.
+  // mysqli_real_escape_string() function escapes special characters in a string and prevents against sql attacks
+
+  // fetch the email & password of the user
+   $filter_email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
+   $email = mysqli_real_escape_string($conn, $filter_email);
+   $filter_pass = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
+   $pass = mysqli_real_escape_string($conn, md5($filter_pass));
+
+
+// _____________________________________________________________________________
+
+   // select all users that have the same email and password that the user just entered from the users table in the databse, if possible
+   $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+
+   // if the rows returned are more than 0, then:
+   if(mysqli_num_rows($select_users) > 0){
+     // Return an associative array of the user's data
+      $row = mysqli_fetch_assoc($select_users);
+
+// _____________________________________________________________________________
+
+      // if the user is an admin, save his credetials in the session and redirect him to the admin page
+      if($row['type'] == 'admin'){
+         $_COOKIE['uid'] = md5($row['UserID']);
+         $_SESSION['admin_name'] = $row['name'];
+         $_SESSION['admin_email'] = $row['email'];
+         $_SESSION['admin_id'] = $row['id'];
+         header('location:adminPage.php');
+
+// _____________________________________________________________________________
+
+      // if the user is not an admin, save his credetials in the session and redirect him to the home page
+      }elseif($row['type'] == 'user'){
+         $_COOKIE['uid'] = md5($row['UserID']);
+         $_SESSION['user_name'] = $row['name'];
+         $_SESSION['user_email'] = $row['email'];
+         $_SESSION['user_id'] = $row['id'];
+         header('location:home.php');
+      }
+
+// _____________________________________________________________________________
+
+      else{
+         $message[] = 'no user found!'; // store notification message
+      }
+    }else{ // if email or password do not match the ones stored in the users table in the database
+      $message[] = 'Incorrect email or password, try again!'; // store notification message
+    }
+}
+// _____________________________________________________________________________
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,21 +73,24 @@
   <title>Login Page</title>
 </head>
 <body>
+
+  <?php include 'Templates/notification.php' ?>
+
   <div class="login-wrapper">
-    <form action="" class="form">
+    <form action="login.php" method="POST" class="form">
       <img src="images/avatar.png" alt="">
       <h2>Login</h2>
       <div class="input-group">
-        <input type="text" name="loginUser" id="loginUser" required>
-        <label for="loginUser">User Name</label>
+        <input type="email" name="email" id="loginUser" required>
+        <label for="email">Email</label>
       </div>
       <div class="input-group">
-        <input type="password" name="loginPassword" id="loginPassword" required>
-        <label for="loginPassword">Password</label>
+        <input type="password" name="password" id="loginPassword" required>
+        <label for="password">Password</label>
       </div>
-      <input type="submit" value="Login" class="submit-btn">
-      <a href="signup.php" class="forgot-pw">New Here? Sign Up</a>
+      <input type="submit" name="submit" value="Login" class="submit-btn">
+      <span class="forgot-pw">New Here? <a href="signup.php">Sign Up</a> </span>
     </form>
   </div>
-</body>
+</body>?
 </html>
--- Website/signup.php
+++ Website/signup.php
@@ -53,25 +53,9 @@ if(isset($_POST['submit'])){
   <title>Sign-Up Page</title>
 </head>
 <body>
-  <!--  ______________________________________________________________________ -->
 
-    <?php
-    // if there is something stored in the message array then:
-    if(isset($message)){
-      // loop over each message
-       foreach($message as $message){
-         // print out the messages as follows:
-          echo '
-          <div class="message">
-             <span>'.$message.'</span>
-             <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
-          </div>
-          ';
-       }
-    }
-    ?>
-
-<!--  ______________________________________________________________________ -->
+  <?php include 'Templates/notification.php' ?>
+  
   <div class="login-wrapper">
     <form action="signup.php" method='post' class="form">
       <img src="images/avatar.png" alt="">
