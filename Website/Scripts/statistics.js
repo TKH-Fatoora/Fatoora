6ea0867907192735fbec2df846a98a2d3a10a7dd
Nour <ns00149@tkh.edu.eg>
2022-11-09 15:56:09 +0200

    Nour & Ahmed Basem: statistics page HTML & PHP & SQL & JS
    Co-Authored-By: null <AhmeddBasemm@users.noreply.github.com>

---
+++ Website/Scripts/statistics.js
@@ -0,0 +1,74 @@
+
+var category = document.getElementById('category').getAttribute('value').split(', ');
+var ctotal = document.getElementById('ctotal').getAttribute('value').split(', ');
+
+category.pop();
+ctotal.pop();
+
+const data = {
+  labels: category,
+  datasets: [{
+    label: 'My First Dataset',
+    data: ctotal,
+    backgroundColor: [
+      '#FFADAD',
+      '#FFD6A5',
+      '#FDFFB6',
+      '#CAFFBF',
+      '#9BF6FF',
+      '#A0C4FF',
+      '#BDB2FF',
+      '#FFC6FF',
+    ],
+    borderColor:'black',
+    hoverOffset: 10
+  }]
+};
+
+const config = {
+  type: 'pie',
+  data: data,
+};
+
+const myChart = new Chart(
+  document.getElementById('myChart'),
+  config
+);
+
+// _______________________________________________________________________
+
+var method = document.getElementById('method').getAttribute('value').split(', ');
+var mtotal = document.getElementById('mtotal').getAttribute('value').split(', ');
+
+method.pop();
+mtotal.pop();
+
+const data2 = {
+  labels: method,
+  datasets: [{
+    label: 'My First Dataset',
+    data: mtotal,
+    backgroundColor: [
+      // '#FFADAD',
+      // '#FFD6A5',
+      '#FDFFB6',
+      '#CAFFBF',
+      // '#9BF6FF',
+      '#A0C4FF',
+      '#BDB2FF',
+      '#FFC6FF',
+    ],
+    borderColor:'black',
+    hoverOffset: 10
+  }]
+};
+
+const config2 = {
+  type: 'pie',
+  data: data2,
+};
+
+const myChart2 = new Chart(
+  document.getElementById('myChart2'),
+  config2
+);
--- Website/statistics.php
+++ Website/statistics.php
@@ -1,14 +1,109 @@
+<?php
+// connection to databse
+@include 'Templates/config.php';
+
+// start user session
+session_start();
+
+// fetching the value for the user id
+$user_id = $_SESSION['user_id'];
+
+// _____________________________________________________________________________
+
+// if user id is not set, then:
+if(!isset($user_id)){
+  // redirect user to log in again
+   header('location:login.php');
+};
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title>Statistics</title>
+    <link rel="stylesheet" href="CSS/statistics.css">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
   </head>
   <body>
     <?php include 'Templates/notification.php' ?>
     <?php include 'Templates/navbar.php' ?>
 
+<!--_______________________________________________________________________  -->
+  <section class="statistics">
+    <h1>Statistics</h1>
+    <div class="container">
+
+      <!--_______________________________________________________________________  -->
+      <?php
+      $select_user_categories = mysqli_query($conn, "SELECT SUM(amount) as ctotal, category FROM expenses WHERE UserID = '$user_id' group by category;") or die('query failed');
+
+      $category = '';
+      $ctotal = '';
+
+      // if the rows returned are more than 0, then:
+      if(mysqli_num_rows($select_user_categories) > 0){
+        // Return an associative array of the user's data
+        while($row = mysqli_fetch_assoc($select_user_categories)){
+          $category = $category.$row['category'].', ';
+          $ctotal = $ctotal.$row['ctotal'].', ';
+        }}
+
+        // for testing only
+        // echo $category;
+        // echo $ctotal;
+        ?>
+
+        <input type="hidden" name="" id="category" value="<?php echo htmlspecialchars($category) ?>">
+        <input type="hidden" name="" id="ctotal" value="<?php echo htmlspecialchars($ctotal) ?>">
+
+        <!--_______________________________________________________________________  -->
+
+        <div class="pieChart">
+          <h3>Expenses per Category</h3>
+          <canvas id="myChart"></canvas>
+        </div>
+
+        <!--_______________________________________________________________________  -->
+
+        <?php
+        $select_user_method = mysqli_query($conn, "SELECT SUM(amount) as mtotal, method FROM expenses WHERE UserID = '$user_id' group by method;") or die('query failed');
+
+        $method = '';
+        $mtotal = '';
+
+        // if the rows returned are more than 0, then:
+        if(mysqli_num_rows($select_user_method) > 0){
+          // Return an associative array of the user's data
+          while($row = mysqli_fetch_assoc($select_user_method)){
+            $method = $method.$row['method'].', ';
+            $mtotal = $mtotal.$row['mtotal'].', ';
+          }}
+
+          // for testing only
+          // echo $method;
+          // echo $mtotal;
+          ?>
+
+          <input type="hidden" name="" id="method" value="<?php echo htmlspecialchars($method); ?>">
+          <input type="hidden" name="" id="mtotal" value="<?php echo htmlspecialchars($mtotal); ?>">
+
+          <!--_______________________________________________________________________  -->
+
+
+          <div class="pieChart">
+            <h3>Expenses per Payment Method</h3>
+            <canvas id="myChart2"></canvas>
+          </div>
+
+          <!--_______________________________________________________________________  -->
+
+    </div>
+
+  </section>
+
 
     <?php include 'Templates/footer.php' ?>
+    <script type="text/javascript" src="Scripts/statistics.js"></script>
   </body>
 </html>
