49074294211812d77b243f01d517825d8e448e60
Nour <ns00149@tkh.edu.eg>
2022-11-09 20:58:11 +0200

    Nour & Ahmed Basem: new SQL DB
    Co-Authored-By: null <AhmeddBasemm@users.noreply.github.com>

--- Website/Database/fatoora.sql
+++ Website/Database/fatoora.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 07, 2022 at 07:18 PM
+-- Generation Time: Nov 09, 2022 at 07:54 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -36,10 +36,33 @@ CREATE TABLE `expenses` (
   `method` varchar(255) NOT NULL,
   `category` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `amount` int(100) NOT NULL,
+  `amount` double NOT NULL,
   `note` varchar(255) NOT NULL,
   `image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `expenses`
+--
+
+INSERT INTO `expenses` (`exID`, `UserID`, `date`, `method`, `category`, `name`, `amount`, `note`, `image`) VALUES
+(14, 0, '2022-11-07', 'debit', 'health', 'Sharaky', 0, 'd', 'none.jpg'),
+(15, 0, '2022-11-07', 'credit', 'selfdevelopment', 'name', 0, 'cd', 'name.jpg'),
+(16, 9, '2022-11-06', 'cash', 'food', 'Tabali', 0, 'Fries', 'none.jpg'),
+(17, 9, '2022-11-03', 'prepaid', 'food', 'Mcdonalds', 0, 'ice cream', 'none.jpg'),
+(18, 9, '2022-11-03', 'prepaid', 'food', 'pick n pack', 0, 'pizza', 'none.jpg'),
+(19, 9, '2022-11-08', 'credit', 'shopping', 'H&M', 0, 't-shirt', 'none.jpg'),
+(20, 10, '2022-11-08', 'debit', 'coffee', 'starbucks', 23242.11, 'latte', 'none.jpg'),
+(21, 9, '2022-11-08', 'other', 'selfdevelopment', 'diwan', 0, 'all the bright places', 'none.jpg'),
+(22, 9, '2022-11-08', 'debit', 'shopping', 'idk', 0, 'oh no', 'none.jpg'),
+(23, 10, '2022-11-07', 'cash', 'shopping', 'waw', 0, 'a', 'none.jpg'),
+(24, 9, '2022-11-06', 'credit', 'coffee', 'vdf', 0, 'dgag', 'none.jpg'),
+(26, 9, '2022-11-03', 'other', 'entertainment', 'test', 123, '', 'none.jpg'),
+(27, 9, '2022-11-01', 'prepaid', 'selfdevelopment', 'book', 56, 'abc', 'none.jpg'),
+(29, 9, '2022-11-04', 'debit', 'other', 'nour', 34, '', 'none.jpg'),
+(30, 9, '2022-12-02', 'credit', 'entertainment', 'name', 89, '', 'none.jpg'),
+(31, 9, '2022-11-24', 'credit', 'transportation', 'hcg', 89, '', 'none.jpg'),
+(32, 9, '2022-11-27', 'cash', 'food', 'n', 1234, '', 'none.jpg');
 
 -- --------------------------------------------------------
 
@@ -49,9 +72,20 @@ CREATE TABLE `expenses` (
 
 CREATE TABLE `message` (
   `MessageID` int(11) NOT NULL,
-  `Content` text NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `content` text NOT NULL,
   `UserID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `message`
+--
+
+INSERT INTO `message` (`MessageID`, `email`, `subject`, `content`, `UserID`) VALUES
+(1, 'v', 'v', '', 1),
+(2, 'v', 'v', 'v', 1),
+(3, 'nour', 'f', 's', 1);
 
 -- --------------------------------------------------------
 
@@ -61,13 +95,22 @@ CREATE TABLE `message` (
 
 CREATE TABLE `users` (
   `UserID` int(11) NOT NULL,
-  `FirstName` varchar(20) NOT NULL,
-  `LastName` varchar(20) NOT NULL,
-  `Email` varchar(100) NOT NULL,
-  `Password` varchar(30) NOT NULL,
-  `Gender` varchar(10) NOT NULL,
-  `Birthdate` date NOT NULL
+  `name` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `birthdate` date NOT NULL,
+  `type` varchar(100) NOT NULL DEFAULT 'user'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`UserID`, `name`, `email`, `password`, `birthdate`, `type`) VALUES
+(7, 'nour', 'n.sharaky@outlook.com', 'ccbc1770bb10486495d127a7d65c252b', '2022-11-07', 'user'),
+(8, 'nour', 'user@gmail.com', 'ee11cbb19052e40b07aac0ca060c23ee', '2022-11-07', 'user'),
+(9, 'nour', 'test@test.com', '098f6bcd4621d373cade4e832627b4f6', '2022-11-08', 'user'),
+(10, 'ahmed', 'a@a.com', '0cc175b9c0f1b6a831c399e269772661', '2022-11-08', 'user');
 
 --
 -- Indexes for dumped tables
@@ -90,7 +133,7 @@ ALTER TABLE `message`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`UserID`),
-  ADD UNIQUE KEY `Email` (`Email`);
+  ADD UNIQUE KEY `Email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -100,19 +143,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `expenses`
 --
 ALTER TABLE `expenses`
-  MODIFY `exID` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `exID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT for table `message`
 --
 ALTER TABLE `message`
-  MODIFY `MessageID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `MessageID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
