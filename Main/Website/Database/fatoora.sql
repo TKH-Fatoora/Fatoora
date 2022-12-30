0e1f23ddf5f9a7cf257722ae8ec25e107acd20c2
AhmeddBasemm <ahmedbasemegy@gmail.com>
2022-12-30 15:23:15 +0200

    Ahmed Basem: - New DB SQL File

--- Main/Website/Database/fatoora.sql
+++ Main/Website/Database/fatoora.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 19, 2022 at 10:54 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Generation Time: Dec 30, 2022 at 02:21 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 7.4.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -46,25 +46,24 @@ CREATE TABLE `expenses` (
 --
 
 INSERT INTO `expenses` (`exID`, `UserID`, `date`, `method`, `category`, `name`, `amount`, `note`, `image`) VALUES
-(14, 9, '2022-11-07', 'debit', 'health', 'medicine', 200, '', 'none.jpg'),
-(15, 9, '2022-11-07', 'credit', 'selfdevelopment', 'course', 1000, 'cd', 'name.jpg'),
-(16, 9, '2022-11-06', 'cash', 'food', 'Tabali', 35, 'Fries', 'none.jpg'),
-(17, 9, '2022-11-03', 'prepaid', 'food', 'Mcdonalds', 120, 'ice cream', 'none.jpg'),
-(18, 9, '2022-11-03', 'prepaid', 'food', 'pick n pack', 246, 'pizza', 'none.jpg'),
-(19, 9, '2022-11-08', 'credit', 'shopping', 'H&M', 350, 't-shirt', 'none.jpg'),
-(20, 10, '2022-11-08', 'debit', 'coffee', 'starbucks', 78.85, 'latte', 'none.jpg'),
-(21, 9, '2022-11-08', 'other', 'selfdevelopment', 'diwan', 250, 'all the bright places', 'none.jpg'),
-(23, 10, '2022-11-07', 'cash', 'shopping', 'shoes', 2000, '', 'none.jpg'),
-(24, 9, '2022-11-06', 'credit', 'coffee', 'coffee lab', 55, '', 'none.jpg'),
-(26, 9, '2022-11-03', 'other', 'entertainment', 'game', 123, '', 'none.jpg'),
-(29, 9, '2022-11-04', 'debit', 'other', 'gift', 34, '', 'none.jpg'),
-(30, 9, '2022-12-02', 'credit', 'entertainment', 'bowling', 89, '', 'none.jpg'),
-(31, 9, '2022-11-24', 'credit', 'transportation', 'uber', 89, '', 'none.jpg'),
-(32, 9, '2022-11-27', 'cash', 'food', 'sushi', 350, '', 'none.jpg'),
-(33, 11, '2022-11-10', 'cash', 'selfdevelopment', 'E-Learn Security', 400, '', 'E-Learn Security.png'),
-(34, 11, '2022-11-10', 'debit', 'coffee', 'Starbucks', 55, '', 'none.jpg'),
-(35, 9, '', 'credit', 'transportation', 'r', 123, 'ds', 'none.jpg'),
-(36, 9, '2022-11-19', 'cash', 'coffee', 'Starbucks', 70, 'choco', 'none.jpg');
+(14, 0, '2022-11-07', 'debit', 'health', 'Sharaky', 0, 'd', 'none.jpg'),
+(15, 0, '2022-11-07', 'credit', 'selfdevelopment', 'name', 0, 'cd', 'name.jpg'),
+(16, 9, '2022-11-06', 'cash', 'food', 'Tabali', 0, 'Fries', 'none.jpg'),
+(17, 9, '2022-11-03', 'prepaid', 'food', 'Mcdonalds', 0, 'ice cream', 'none.jpg'),
+(18, 9, '2022-11-03', 'prepaid', 'food', 'pick n pack', 0, 'pizza', 'none.jpg'),
+(19, 9, '2022-11-08', 'credit', 'shopping', 'H&M', 0, 't-shirt', 'none.jpg'),
+(20, 10, '2022-11-08', 'debit', 'coffee', 'starbucks', 23242.11, 'latte', 'none.jpg'),
+(21, 9, '2022-11-08', 'other', 'selfdevelopment', 'diwan', 0, 'all the bright places', 'none.jpg'),
+(22, 9, '2022-11-08', 'debit', 'shopping', 'idk', 0, 'oh no', 'none.jpg'),
+(23, 10, '2022-11-07', 'cash', 'shopping', 'waw', 0, 'a', 'none.jpg'),
+(24, 9, '2022-11-06', 'credit', 'coffee', 'vdf', 0, 'dgag', 'none.jpg'),
+(26, 9, '2022-11-03', 'other', 'entertainment', 'test', 123, '', 'none.jpg'),
+(27, 9, '2022-11-01', 'prepaid', 'selfdevelopment', 'book', 56, 'abc', 'none.jpg'),
+(29, 9, '2022-11-04', 'debit', 'other', 'nour', 34, '', 'none.jpg'),
+(30, 9, '2022-12-02', 'credit', 'entertainment', 'name', 89, '', 'none.jpg'),
+(31, 9, '2022-11-24', 'credit', 'transportation', 'hcg', 89, '', 'none.jpg'),
+(32, 9, '2022-11-27', 'cash', 'food', 'n', 1234, '', 'none.jpg'),
+(33, 11, '2022-03-02', 'credit', 'transportation', 'Uber', 150, 'From Home To Uni', 'Uber.jpg');
 
 -- --------------------------------------------------------
 
@@ -85,10 +84,9 @@ CREATE TABLE `message` (
 --
 
 INSERT INTO `message` (`MessageID`, `email`, `subject`, `content`, `UserID`) VALUES
-(2, 'test@test.com', 'quesion', 'Can i have multiple accounts?', 9),
-(3, 'a@a.com', 'wow', 'this webapp is very useful ', 10),
-(4, 'test@test.com', 'hi', 'bye', 9),
-(6, 'test@test.com', 'Concern', 'Hello World!', 9);
+(1, 'v', 'v', '', 1),
+(2, 'v', 'v', 'v', 1),
+(3, 'nour', 'f', 's', 1);
 
 -- --------------------------------------------------------
 
@@ -102,18 +100,21 @@ CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `birthdate` date NOT NULL,
-  `type` varchar(100) NOT NULL DEFAULT 'user'
+  `type` varchar(100) NOT NULL DEFAULT 'user',
+  `2fa` varchar(255) NOT NULL,
+  `QR` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`UserID`, `name`, `email`, `password`, `birthdate`, `type`) VALUES
-(9, 'nour', 'test@test.com', '098f6bcd4621d373cade4e832627b4f6', '2022-11-08', 'user'),
-(10, 'ahmed', 'a@a.com', '0cc175b9c0f1b6a831c399e269772661', '2022-11-08', 'admin'),
-(12, 'adminn', 'admin@gmail.com', '21232f297a57a5a743894a0e4a801fc3', '2022-11-16', 'admin'),
-(13, 'Employee', 'e@e.com', '47e2e8c3fdb7739e740b95345a803cac', '2022-11-19', 'employee');
+INSERT INTO `users` (`UserID`, `name`, `email`, `password`, `birthdate`, `type`, `2fa`, `QR`) VALUES
+(7, 'nour', 'n.sharaky@outlook.com', 'ccbc1770bb10486495d127a7d65c252b', '2022-11-07', 'user', '', 0),
+(8, 'nour', 'user@gmail.com', 'ee11cbb19052e40b07aac0ca060c23ee', '2022-11-07', 'user', '', 0),
+(9, 'nour', 'test@test.com', '098f6bcd4621d373cade4e832627b4f6', '2022-11-08', 'user', 'PU6LSE6EVF3JSUSV', 1),
+(10, 'ahmed', 'a@a.com', '0cc175b9c0f1b6a831c399e269772661', '2022-11-08', 'admin', 'BTJHDRG73DQDIUFL', 0),
+(11, 'ahmed', 'a_basem@hotmail.com', '202cb962ac59075b964b07152d234b70', '2003-03-23', 'user', '', 0);
 
 --
 -- Indexes for dumped tables
@@ -146,19 +147,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `expenses`
 --
 ALTER TABLE `expenses`
-  MODIFY `exID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `exID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `message`
 --
 ALTER TABLE `message`
-  MODIFY `MessageID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `MessageID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
