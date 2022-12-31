7d838954cff6e5d0a619c936a41d532bca98e36b
AhmeddBasemm <ahmedbasemegy@gmail.com>
2022-12-31 15:28:13 +0200

    Ahmed Basem -Bug Fix

--- Main/Website/Templates/HackingDetectedTemp.php
+++ Main/Website/Templates/HackingDetectedTemp.php
@@ -1,9 +1,9 @@
 <?php
 
-function Hacking_Detected($Reason,$SOCmsg)
+function Hacking_Detected($Usermsg,$SOCmsg)
 {
   // Submit Reason to SOC DB
-  $_SESSION["HACK"] = $Reason;
+  $_SESSION["HACK"] = $Usermsg;
   header('location:Hacking_Detected.php');
 }
  ?>
