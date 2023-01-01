aa29aa67a31318e8fa80a431f1c219cde9ac9f7f
Nour <ns00149@tkh.edu.eg>
2023-01-02 01:10:44 +0200

    Nour: adminDash.php additional boxes and comments

--- Main/Website/adminDash.php
+++ Main/Website/adminDash.php
@@ -4,6 +4,7 @@
 
 // start admin session
 session_start();
+
 // prevent session hijacking
 @include 'Templates/session_hijacking_prevention.php';
 
@@ -14,7 +15,7 @@ $admin_id = $_SESSION['admin_id'];
 
 // if admin id is not set, then:
 if(!isset($admin_id)){
-  // redirect user to log in page again
+  // log user out
    header('location:logout.php');
 };
 
@@ -28,14 +29,16 @@ if(!isset($admin_id)){
   <head>
     <meta charset="utf-8">
     <title>Dashboard</title>
+    <!-- css style sheet link -->
+    <link rel="stylesheet" href="CSS/adminDash_styles.css">
     <!-- set content's width according to current screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- linking my fontawesome kit to be able to add icons  -->
+    <!-- linking fontawesome kit to be able to add icons  -->
     <script src="https://kit.fontawesome.com/51f1a2fdea.js" crossorigin="anonymous"></script>
-    <!-- css style sheet link -->
-    <link rel="stylesheet" href="CSS/adminDash_styles.css">
   </head>
+
+<!-- _______________________________________________________________________ -->
 
   <body>
     <?php @include 'Templates/adminNav.php'; ?>
@@ -61,13 +64,26 @@ if(!isset($admin_id)){
         <div class="box">
            <?php
             // select all users that have an employee account type in the users table in the databse
-            $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE type = 'employee'") or die('query failed');
+            $select_employee = mysqli_query($conn, "SELECT * FROM `users` WHERE type = 'employee'") or die('query failed');
             // counts the number of rows returned ==> number of employee accounts
-            $number_of_users = mysqli_num_rows($select_users);
+            $number_of_employee = mysqli_num_rows($select_employee);
            ?>
            <!-- print out the number of employee accounts -->
-           <h3><?php echo htmlspecialchars($number_of_users); ?></h3> <!-- htmlspecialchars prevents against xss attacks -->
+           <h3><?php echo htmlspecialchars($number_of_employee); ?></h3> <!-- htmlspecialchars prevents against xss attacks -->
            <p>Employee Users</p>
+        </div>
+<!-- _______________________________________________________________________ -->
+        <!-- Number of Security admin accounts -->
+        <div class="box">
+           <?php
+            // select all users that have a security account type in the users table in the databse
+            $select_security = mysqli_query($conn, "SELECT * FROM `users` WHERE type = 'security'") or die('query failed');
+            // counts the number of rows returned ==> number of security admin accounts
+            $number_of_security = mysqli_num_rows($select_security);
+           ?>
+           <!-- print out the number of security admin accounts -->
+           <h3><?php echo htmlspecialchars($number_of_security); ?></h3> <!-- htmlspecialchars prevents against xss attacks -->
+           <p>Security Admin Users</p>
         </div>
 <!-- _______________________________________________________________________ -->
         <!-- Number of admin accounts -->
@@ -96,11 +112,36 @@ if(!isset($admin_id)){
            <p>Total Accounts</p>
         </div>
 <!-- _______________________________________________________________________ -->
-
+        <!-- Total number of messages -->
+        <div class="box">
+           <?php
+            // select all messages in the message table in the databse
+            $select_messages = mysqli_query($conn, "SELECT * FROM `message`") or die('query failed');
+            // count number of rows returned ==> total messages recieved
+            $number_of_messages = mysqli_num_rows($select_messages);
+           ?>
+           <!-- print out the total number of messages -->
+           <h3><?php echo htmlspecialchars($number_of_messages); ?></h3> <!-- htmlspecialchars prevents against xss attacks -->
+           <p>Messages</p>
+        </div>
+<!-- _______________________________________________________________________ -->
+        <!-- Total number of Alerts -->
+        <div class="box">
+           <?php
+            // select all alerts in the secalerts table in the databse
+            $select_alerts = mysqli_query($conn, "SELECT * FROM `secalerts`") or die('query failed');
+            // count number of rows returned ==> total alerts recieved
+            $number_of_alerts = mysqli_num_rows($select_alerts);
+           ?>
+           <!-- print out the total number of alerts -->
+           <h3><?php echo htmlspecialchars($number_of_alerts); ?></h3> <!-- htmlspecialchars prevents against xss attacks -->
+           <p>Security Alerts</p>
+        </div>
+<!-- _______________________________________________________________________ -->
       </div>
     </section>
 
     <!-- linking the JavaScript file -->
-    <script src="js/admin_script.js"></script>
+    <script src="Scripts/nav.js"></script>
   </body>
 </html>
