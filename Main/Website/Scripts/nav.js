15117a7bb3c8068fbf36210b552a54aca92a613d
Nour <ns00149@tkh.edu.eg>
2023-01-02 00:11:43 +0200

    Nour: commented navbar.js

--- Main/Website/Scripts/nav.js
+++ Main/Website/Scripts/nav.js
@@ -1,8 +1,12 @@
+
+// selectng the divs for the navigation bar and the account info window
 let user = document.querySelector('.header .flex .account');
 let navbar = document.querySelector('.header .flex .navbar');
 
-if (document.querySelector('#menu-btn')){
+// _____________________________________________________________________________
 
+// if the menu button is clicked, display the nav bar content and hide the account info window
+if (document.querySelector('#menu-btn')){
   document.querySelector('#menu-btn').addEventListener("click", () =>
   {
     navbar.classList.toggle('active');
@@ -10,8 +14,10 @@ if (document.querySelector('#menu-btn')){
   });
 }
 
-if (document.querySelector('#user-btn')){
+// _____________________________________________________________________________
 
+// if the account info button is clicked, display the account info content and hide the nav bar
+if (document.querySelector('#user-btn')){
   document.querySelector('#user-btn').addEventListener("click", () =>
   {
     user.classList.toggle('active');
@@ -19,6 +25,9 @@ if (document.querySelector('#user-btn')){
   });
 }
 
+// _____________________________________________________________________________
+
+// if the user scrolled, hide both the nav bar content and the account info content
 window.onscroll = () =>{
    navbar.classList.remove('active');
    user.classList.remove('active');
